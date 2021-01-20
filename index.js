c6d7f135097d1d99e685c57e3b9737c02608d420
William Bryan <wwbryan00@gmail.com>
2021-01-19 21:05:40 -0800

    add questions and dependencies

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const util = require('util');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is your GitHub username?",
+    "What is your email address?",
+    "What is your projects name?",
+    "Please write a short description of your project",
+    "What kind of license should your project have?",
+    "What command should be run to install dependencies?",
+    "What command should be run to run tests?",
+    "What doe the user need to know about using the repo?",
+    "What does the user need to know about contributing to the repo?"
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
